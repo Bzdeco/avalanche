9262f3b52309a4ab8574a1933f17609585985125
Maciej Sudoł <maciejsudol@gmail.com>
2016-11-19 13:55:49 +0100

    Gradle support

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
 
         primaryStage.setTitle("Projekt lawinowy");
         primaryStage.setScene(new Scene(root));
