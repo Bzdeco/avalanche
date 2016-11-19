714887a796e6d46bc5d0c418093ca26613cc9321
Paweł Inglot <igi21@gazeta.pl>
2016-11-19 19:02:47 +0100

    unimportant

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
-        logger.info("logging test");
+        logger.info("logging test.Teskt {} ze zmiennymi {}", 1, 2);
         primaryStage.setTitle("Projekt lawinowy");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
