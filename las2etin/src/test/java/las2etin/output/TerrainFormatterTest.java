aafd8fd0760ea69657004486ee864a3c2d448f3d
Kuba Gwizdała <jakub.gwizdala@gmail.com>
2017-12-17 16:11:11 +0100

    Testing serialization and deserialization of real las file.

--- las2etin/src/test/java/las2etin/output/TerrainFormatterTest.java
+++ las2etin/src/test/java/las2etin/output/TerrainFormatterTest.java
@@ -1,5 +1,6 @@
 package las2etin.output;
 
+import las2etin.las.LASFile;
 import las2etin.las.LASReader;
 import las2etin.las.vertex.Bounds;
 import las2etin.model.Terrain;
@@ -8,6 +9,7 @@ import las2etin.model.TerrainSettings;
 import las2etin.model.TerrainSettingsBuilder;
 import las2etin.tin.Tin;
 import las2etin.tin.TinBuilder;
+import org.junit.Ignore;
 import org.junit.Test;
 import testutil.TestUtil;
 import tinfour.common.Vertex;
@@ -45,6 +47,32 @@ public class TerrainFormatterTest
 
         // when
         Path saveLocation = Paths.get("src/test/resources/terrain.ser");
+        Path serializedTerrain = TerrainFormatter.serialize(terrainToSerialize, saveLocation);
+        Terrain deserializedTerrain = TerrainFormatter.deserialize(serializedTerrain);
+
+        // then
+        assertThat(terrainToSerialize).isEqualTo(deserializedTerrain);
+    }
+
+    @Test
+    @Ignore("Serialization and deserializtion of real .las files, takes very long")
+    public void serializeAndDeserializeTestLasFile() throws Exception
+    {
+        // given
+        LASFile file = LASFile.fromFilePath("src/test/resources/test.las");
+        LASReader reader = LASReader.createFor(file);
+        List<Vertex> readVertices = reader.getVerticesRecords();
+        Bounds bounds = reader.getVerticesBounds();
+        Tin terrainMesh = new TinBuilder().withVertices(readVertices).withBounds(bounds).build();
+        int widthInCells = 1000;
+        int heightInCells = 1000;
+        TerrainSettings settings = new TerrainSettingsBuilder().withWidthInCells(widthInCells)
+                                                               .withHeightInCells(heightInCells)
+                                                               .build();
+        Terrain terrainToSerialize = new TerrainBuilder(terrainMesh).withSettings(settings).build();
+
+        // when
+        Path saveLocation = Paths.get("src/test/resources/test.ser");
         Path serializedTerrain = TerrainFormatter.serialize(terrainToSerialize, saveLocation);
         Terrain deserializedTerrain = TerrainFormatter.deserialize(serializedTerrain);
 
